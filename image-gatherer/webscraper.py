e168ea0edb282de287baac5835a11f5318571994
adamgate <applegateadaml@gmail.com>
2023-04-19 10:58:39 -0600

    Migrated console to rich text

--- image-gatherer/webscraper.py
+++ image-gatherer/webscraper.py
@@ -12,8 +12,10 @@ from PIL import Image
 import io
 import requests
 from selenium import webdriver
-from selenium.webdriver.common import keys
 from selenium.webdriver.common.by import By
+from rich.progress import track
+
+from image_gatherer import console
 
 # Load chromedriver for selenium
 config_file = str(Path('.').parent.absolute())
@@ -43,51 +45,51 @@ def fetch_images(query: str, num: int, headless: bool):
     time.sleep(3)
 
     # scroll to the bottom of the page so many images load
-    print("Loading images...")
     last_height = driver.execute_script('return document.body.scrollHeight')
-    while True:
-        driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+    with console.status("[green]Finding images from google...", spinner='bouncingBar') as status:
+        while True:
+            driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
 
-        # If it isn't loading many images, increase this number
-        # time.sleep(1)
-
-        new_height = driver.execute_script('return document.body.scrollHeight')
-
-        # click on "Show more results" if it's there
-        try:
-            driver.find_element(By.CSS_SELECTOR, ".YstHxe input").click()
+            # If it isn't loading many images, increase this number
             time.sleep(3)
-        except:
-            pass
 
-        # checking if we have reached the bottom of the page
-        if new_height == last_height:
-            break
+            new_height = driver.execute_script('return document.body.scrollHeight')
 
-        last_height = new_height
+            # click on "Show more results" if it's there
+            try:
+                driver.find_element(By.CSS_SELECTOR, ".YstHxe input").click()
+                time.sleep(3)
+            except:
+                pass
+
+            # checking if we have reached the bottom of the page
+            if new_height == last_height:
+                break
+
+            last_height = new_height
 
     # Grab all of the image thumbnails
     thumbnail_results = driver.find_elements(By.CLASS_NAME, 'rg_i')
-    print(f"Found {len(thumbnail_results)} potential images.")
+    console.print(f"Found {len(thumbnail_results)} potential images.")
     fullsize_images = []
 
     # load the large version of each image and save it
     count = 0
-    for thumbnail in thumbnail_results:
+    for thumbnail in track(thumbnail_results, total=num, description=f"Getting {num} images..."):
         thumbnail.click()
         time.sleep(1)
 
         full_image = driver.find_element(By.CLASS_NAME, 'r48jcc')
         if full_image.get_attribute('src') and 'http' in full_image.get_attribute('src'):
             fullsize_images.append(full_image.get_attribute('src'))
-            print(f"Loaded image #{count+1} of {num}: {full_image.get_attribute('src')}")
+            # console.print(f"Loaded image #{count+1} of {num}: {full_image.get_attribute('src')}")
             count += 1
 
         if count == num:
             break
 
     if count < num:
-        print(f"Couldn't get {num} images. Got {len(fullsize_images)} instead.")
+        console.print(f"[red]Couldn't get {num} images. Got {len(fullsize_images)} instead.")
 
     driver.quit()
 
@@ -98,23 +100,24 @@ def save_images(image_links,  query: str, path: Path):
     """ Loads image links into images and saves them to the provided path.  """
 
     count = 0
-    for link in image_links:
+    for link in track(image_links, total=len(image_links), description=f"Saving {len(image_links)} images..."):
         img_path = f"{path}/{query}{count+1}.jpg"
 
         try:
             image_content = requests.get(link).content
 
         except Exception as e:
-            print(f"Error downloading image: {link} - {e}")
+            # console.print(f"Error downloading image: {link} - {e}")
+            pass
 
         try:
             image_file = io.BytesIO(image_content)
             image = Image.open(image_file).convert('RGB')
             with open(img_path, 'wb') as file:
                 image.save(file, "JPEG", quality=85)
-            print(f"Successfully saved img #{count+1} - as {img_path}")
+            # console.print(f"Successfully saved img #{count+1}.")
 
         except Exception as e:
-            print(f"Error saving img: #{count+1} - {e}")
+            console.print(f"Error saving img: #{count+1} - {e}")
 
         count += 1
